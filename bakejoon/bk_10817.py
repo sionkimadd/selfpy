289783b95a76cdddf6e3eb544c542d1079967dbc
Sion Kim <skim14@academic.rrc.ca>
2024-01-12 23:44:17 -0600

    24.01.12(6)

--- bakejoon/bk_10817.py
+++ bakejoon/bk_10817.py
@@ -1,13 +1,13 @@
 # 10817_wash face
-# A, B, C = map(int,input().split())
-# if all(1 <= x <= 100 for x in (A, B, C)):
-#     if B<=A<=C or C<=A<=B:
-#         print(A)
-#     elif A<=B<=C or C<=B<=A:
-#         print(B)
-#     elif A<=C<=B or B<=C<=A:
-#         print(C)
-#     else:
-#         print(A)
-# else:
-#     print("error")
+A, B, C = map(int,input().split())
+if all(1 <= x <= 100 for x in (A, B, C)):
+    if B<=A<=C or C<=A<=B:
+        print(A)
+    elif A<=B<=C or C<=B<=A:
+        print(B)
+    elif A<=C<=B or B<=C<=A:
+        print(C)
+    else:
+        print(A)
+else:
+    print("error")
---
+++ bakejoon/bk_9498.py
@@ -0,0 +1,15 @@
+# 9498_test scores
+S = int(input())
+if S>=0 and S<=100:
+    if S>=90 and S<=100:
+        print("A")
+    elif S>=80 and S<90:
+        print("B")
+    elif S>=70 and S <80:
+        print("C")
+    elif S>=60 and S <70:
+        print("D")
+    else:
+        print("F")
+else:
+    print("error")
